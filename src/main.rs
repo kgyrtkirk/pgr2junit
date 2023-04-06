48fb31ac0da8b8a3e1cccf365465bc0e2cb9be9e
Zoltan Haindrich <zoltan@timescale.com>
2023-04-06 08:44:51 +0000

    add test data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+
+use crate::pgr_model::Model;
+
+mod pgr_model;
+
+
 fn main() {
+    let a: Model;
+
     println!("Hello, world!");
 }
--- src/pgr_model.rs
+++ src/pgr_model.rs
@@ -1,27 +1,21 @@
+// Make sure you add the `patch` crate to the `[dependencies]` key of your Cargo.toml file.
+use patch::Patch;
 
+pub struct Model {}
 
-// // Make sure you add the `patch` crate to the `[dependencies]` key of your Cargo.toml file.
-// use patch::Patch;
+impl Model {
+    pub fn new(dir: &str) -> Model {
+        return Model {};
+    }
+}
 
-// let sample = "\
-// --- before.py
-// +++ path/to/after.py
-// @@ -1,4 +1,4 @@
-// -bacon
-// -eggs
-// -ham
-// +python
-// +eggy
-// +hamster
-//  guido\n";
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-// let patch = Patch::from_single(sample)?;
-// assert_eq!(&patch.old.path, "before.py");
-// assert_eq!(&patch.new.path, "path/to/after.py");
+    #[test]
+    fn test1() {
 
-// // Print out the parsed patch file in its Rust representation
-// println!("{:#?}", patch);
-
-// // Print out the parsed patch file in the Unified Format. For input that was originally in the
-// // Unified Format, this will produce output identical to that original input.
-// println!("{}", patch); // use format!("{}\n", patch) to get this as a String
+        // assert_eq!(Solution::count_time(String::from("?5:00")),2);
+    }
+}
